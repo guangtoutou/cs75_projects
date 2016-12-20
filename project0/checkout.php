a0111ee4ba98a5369df4b90af6a6a21007d04c75
guangtoutou <ni.ningning@gmail.com>
2016-12-20 23:06:50 +0800

    create a xml file of menu
    start with sample data, including 3 categories.
    3 dishes for each category.

---
+++ project0/checkout.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html lang="zh-CN">
+<head>
+	<title>Check Out</title>
+	<link href="http://cdn.bootcss.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+	Check Out
+	</header>
+
+</body>
+</html>
+
---
+++ project0/menu.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+<html lang="zh-CN">
+<head>
+	<title>Menu</title>
+	<link href="http://cdn.bootcss.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+	menu
+	</header>
+
+	<?php
+		$xml = simplexml_load_file("menu.xml");
+		$result = $xml->xpath("dish");
+		print_r($result);
+	?>
+
+
+</body>
+</html>
+
