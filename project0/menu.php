65bd1f73df7b9df60fd681e12cee2e332ff59465
unknown <nnni@chq.app.com.cn>
2016-12-21 09:13:27 +0800

    create menu and checkout page from company laptop

---
+++ project0/menu.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html lang="zh-CN">
+<head>
+	<title>Menu</title>
+	<link href="http://cdn.bootcss.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+	<header>
+		<h1>Menu</h1>
+
+	</header>
+
+</body>
+</html>
