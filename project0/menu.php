742d36bb0ad66f63c3df199f25f012baeebff54c
James Ni <jamesni@app.com.cn>
2016-12-27 17:32:02 +0800

    set the sidebar to hidden on small screens

--- project0/menu.php
+++ project0/menu.php
@@ -15,16 +15,24 @@
 			<?php
 				$menu = simplexml_load_file('menu.xml');
 				foreach($menu->category as $category){
+					echo '<div class="row"><div class="col-md-12">';
 					echo "<div class='CategoryTitle' id='${category['seq']}'> ${category['name']}</div>";
-					echo '<div style="display: flex;flex-wrap:wrap">';
-					foreach($category->dish as $dish){
+					echo '</div></div>';
+
+					echo '<div class="row"><div class="col-md-12">';
+					/*foreach($category->dish as $dish){
 						echo "<div class='DishBlock'> <img src='http://placehold.it/160X160'>${dish['name']}</div>";	
+					}*/
+					for($i=0; $i<count($category->dish); $i++){
+						$dish = $category->dish[$i];
+						echo "<div class='DishBlock'> <img src='http://placehold.it/160X160'>${dish['name']}</div>";
+						echo ($category->dish[$i]['name']);
 					}
-					echo '</div>';
+					echo '</div></div>';
 				}
 			?>
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 hidden-sm hidden-xs">
 				<nav id="myScrollspy">
 					<ul class="nav nav-pills nav-stacked">
 						<li><a href="#1">Pizza</a></li>
@@ -32,12 +40,12 @@
 						<li><a href="#3">Special dinners</a></li>
 					</ul>
 				</nav>
-				<div style="position: fixed; bottom: 20px" class="btn-group">
-					<button type="button" class="btn btn-primary">Shopping Cart</button>
-					<button type="button" class="btn btn-primary">Check Out</button>
-				</div>
 			</div>
 		</div>
+	</div>
+	<div style="position: fixed; bottom: 20px; right: 20px;" class="btn-group">
+		<button type="button" class="btn btn-primary">Shopping Cart</button>
+		<button type="button" class="btn btn-primary">Check Out</button>
 	</div>
 </body>
 </html>
